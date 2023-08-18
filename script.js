c10c27a992f67516a43470bf00a622b8bf6174eb
BekirLee <nihadbekirli8@gmail.com>
2023-08-18 23:54:12 +0400

    slick-nav

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".slicknav_menu").addEventListener("click", function () {
+    var menulinks = document.querySelector(".menu_links")
+    menulinks.classList.toggle("active");
+
+})
+
