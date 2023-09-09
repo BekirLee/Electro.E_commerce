f7245b51db48fa09b6b4c2e2733420a4fc7b04bd
BekirLee <nihadbekirli8@gmail.com>
2023-09-09 23:16:57 +0400

    codes for updates on products.js

--- product.js
+++ product.js
@@ -4,15 +4,15 @@ const products = [
         info: {
             price: "$49.00",
             discount: "$44.00",
-            brand: "Apple",
-            category: "phone",
+            brand: "Newest technology",
+            category: "Home",
             img: "img/beardcleaner.jpg",
             hover: "img/bill_image.jpg",
             infoImg1: "img/phone_info1.jpg",
             infoImg2: "img/phone_info2.jpg",
             infoImg3: "img/phone_info2.jpg",
         },
-        name: "Washingmachines",
+        name: "Beard cleaner",
         image: "img/washingmachine_image.jpg",
         href: "pages/products.html"
     },
@@ -21,15 +21,15 @@ const products = [
         info: {
             price: "$249.00",
             discount: "$219.00",
-            brand: "Apple",
-            category: "phone",
+            brand: "Tefal",
+            category: "Home",
             img: "img/amazon_product.jpg",
             hover: "img/background_amazon_product.jpg",
             infoImg1: "img/amazon_product2.jpg",
             infoImg2: "img/background_amazon_product.jpg",
             infoImg3: "img/amazon_product.jpg",
         },
-        name: "Washingmachines",
+        name: "Tefal",
         image: "img/washingmachine_image.jpg",
         href: "pages/products.html"
     },
@@ -181,6 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function info() {
 
     let good = document.querySelector(".gallery-wrap");
+    let feautures = document.querySelector(".feautures_ul");
     const productId = new URLSearchParams(window.location.search).get('id');
 
     if (!productId) {
@@ -217,29 +218,14 @@ function info() {
     </div > 
     `;
 
-    // for (var information of products) {
-    //     var elements = `
-    // <div data-id="${information.id}" class="gallery product-link">
-    //      <div class="img-big-wrap img-thumbnail">
-    //         <a href="${information.info.img}" data-type="image" data-fslightbox="mygallery">
-    //         <img src="${information.info.img}" alt="">
-    //         </a>
-    //     </div>
-    //     <div class="thumbs-wrap pt-2 " style="text-align: center;">
+    feautures.innerHTML = `
+                        <li class="feauture">${product.info.price}</li>
+                        <li class="feauture">${product.info.discount} </li>
+                        <li class="feauture">${product.name}</li>
+                        <li class="feauture">${product.info.category}</li>
+                        <li class="feauture"> ${product.info.brand}</li>
+    `;
 
-    //     <a href="${information.infoImg1}" data-type="image" data-fslightbox="mygallery">
-    //         <img width="60" height="60" src="${information.info.infoImg1}" alt="">
-    //     </a>
-    //     <a href="${information.info.infoImg2}" data-type="image" data-fslightbox="mygallery">
-    //         <img width="60" height="60" src="${information.info.infoImg2}" alt="">
-    //     </a>
-    //     <a href="${information.info.infoImg3}" data-type="image" data-fslightbox="mygallery">
-    //         <img width="60" height="60" src="${information.info.infoImg3}" alt="">
-    //     </a>
-    //     </div>
-    // </div > `;
 
-    //     good.insertAdjacentHTML("beforeend", elements);
-    // }
 }
 // last in here
