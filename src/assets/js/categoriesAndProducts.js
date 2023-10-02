d1cde663561f110d8b33c07901681db4a7a157ab
BekirLee <nihadbekirli8@gmail.com>
2023-10-03 00:21:39 +0400

    reset all pages

--- src/assets/js/categoriesAndProducts.js
+++ src/assets/js/categoriesAndProducts.js
@@ -2,20 +2,22 @@
 
 const categories = [
     {
-        id: 14,
+        id: 1,
         mainPhoto: "",
         name: 'Phone',
-        img: "img/airconditioner_image.jpg",
+        img: "./src/assets/img/airconditioner_image.jpg",
     },
     {
-        id: 345,
-        name: 'TV', mainPhoto: "",
-        img: "img/airconditioner_image.jpg",
+        id: 2,
+        mainPhoto: "",
+        name: 'TV',
+        img: "./src/assets/img/airconditioner_image.jpg",
     },
     {
         id: 3,
+        mainPhoto: "",
         name: "Air-conditioners",
-        img: "img/airconditioner_image.jpg",
+        img: "./src/assets/img/airconditioner_image.jpg",
     },
 
 ]
@@ -28,8 +30,8 @@ var productsSurfaceInfo = [
         info: {
             price: 49.00,
             discount: 44.00,
-            img: "img/beardcleaner.jpg",
-            hover: "img/beardsheaver.jpeg",
+            img: "./src/assets/img/beardcleaner.jpg",
+            hover: "./src/assets/beardsheaver.jpeg",
         },
         category: "Kitchen",
         firstname: "Multigroomer All-in-One Trimmer Series 5000, 23 Piece Mens Grooming Kit",
@@ -40,8 +42,8 @@ var productsSurfaceInfo = [
         info: {
             price: 249.00,
             discount: 219.00,
-            img: "img/amazon_product.jpg",
-            hover: "img/background_amazon_product.jpg",
+            img: "./src/assets/img/amazon_product.jpg",
+            hover: "./src/assets/background_amazon_product.jpg",
         },
         category: "Kitchen",
         firstname: "  Smart Speaker with Alexa Voice Control Built-in Compact Size with Incredible Sound for Any Room",
@@ -52,8 +54,8 @@ var productsSurfaceInfo = [
         info: {
             price: 229.00,
             discount: 209.00,
-            img: "img/amazon_product2.jpg",
-            hover: "img/background_amazon_product.jpg",
+            img: "./src/assets/img/amazon_product2.jpg",
+            hover: "./src/assets/background_amazon_product.jpg",
         },
         category: "Kitchen",
         firstname: "  Home Speaker 500: Smart Bluetooth Speaker with Alexa Voice Control Built-In, White",
@@ -64,12 +66,12 @@ var productsSurfaceInfo = [
         info: {
             price: 1499.00,
             discount: 1399.00,
-            img: "img/computer.jpg",
-            hover: "img/background_computer.jpg",
+            img: "./src/assets/img/computer.jpg",
+            hover: "./src/assets/background_computer.jpg",
         },
         category: "Laptop",
         firstname: " 13 Ultrabook Gaming Laptop: Intel Core i7-1165G7 4 Core, NVIDIA GeForce GTX 1650 Ti Max-Q, 13.3″ 1080p 120Hz, 16GB RAM, 512GB SSD, CNC Aluminum, Chroma RGB, Thunderbolt 4",
-        img: "img/washingmachine_image.jpg",
+        img: "./src/assets/img/washingmachine_image.jpg",
         href: "pages/products.html"
     },
     {
@@ -77,8 +79,8 @@ var productsSurfaceInfo = [
         info: {
             price: 1029.00,
             discount: 999.00,
-            img: "img/computer2.jpg",
-            hover: "img/background_computer.jpg",
+            img: "./src/assets/img/computer2.jpg",
+            hover: "./src/assets/background_computer.jpg",
         },
         category: "Laptop",
         firstname: "15.6″ FHD Display Laptop – Intel i7 – Intel HD Graphics 6000 , Webcam, WiFi, Bluetooth, HDMI, Windows 11,Grey",
@@ -89,8 +91,8 @@ var productsSurfaceInfo = [
         info: {
             price: 699.00,
             discount: 659.00,
-            img: "img/tablet.jpg",
-            hover: "img/phone_image.jpg",
+            img: "./src/assets/img/tablet.jpg",
+            hover: "./src/assets/phone_image.jpg",
         },
         category: "Laptop",
         firstname: "Note 10 Pro 128GB 6GB RAM Factory Unlocked (GSM ONLY) International Model",
@@ -101,8 +103,8 @@ var productsSurfaceInfo = [
         info: {
             price: 1299.00,
             discount: 1199.00,
-            img: "img/phone1.jpg",
-            hover: "img/phonebackground_img.jpg",
+            img: "./src/assets/img/phone1.jpg",
+            hover: "./src/assets/phonebackground_img.jpg",
         },
         category: "phone",
         firstname: "5G Unlocked Smartphone,12GB RAM+256GB Storage120Hz Fluid Display Hasselblad Quad Camera 65W Ultra Fast Charge 50W Wireless Charge",
@@ -113,8 +115,8 @@ var productsSurfaceInfo = [
         info: {
             price: 1099.00,
             discount: 999.00,
-            img: "img/phone2.jpg",
-            hover: "img/phonebackground_img.jpg",
+            img: "./src/assets/img/phone2.jpg",
+            hover: "./src/assets/phonebackground_img.jpg",
         },
         category: "phone",
         firstname: "5G Factory Unlocked Android Cell Phone 128GB Pro-Grade Camera 30X Space Zoom Night Mode, Space Grey",
