80ac0e587e9628d9841ca293dd4b9847da9247bb
BekirLee <nihadbekirli8@gmail.com>
2023-10-02 23:31:48 +0400

    reset all pages

---
+++ src/assets/js/categories.js
@@ -0,0 +1,29 @@
+
+getProducts();
+
+function getProducts() {
+    var elements = document.querySelector(".productsBox");
+
+    if (!elements) {
+        console.error(".productsBox elementi bulunamadi.");
+        return;
+    }
+
+    for (var category of categories) {
+        // phone_count //new 
+        let productCount = productsSurfaceInfo.filter(x => x.categoryid == category.id).length;
+        let element = `
+        <a href="./src/productInfo.html?id=${category.id}">
+            <div class="product" id="${category.id}" >
+                <img src="${category.img}" alt="">
+                
+                <div class="div">
+                    <h2>
+                        ${category.name} <p>${productCount} products</p>
+                    </h2>
+                </div>
+            </div>
+        </a>`;
+        elements.insertAdjacentHTML("beforeend", element);
+    }
+}
