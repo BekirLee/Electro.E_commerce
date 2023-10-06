a5ff96cfc7693e5572351b5017d768fd9b357e5a
BekirLee <nihadbekirli8@gmail.com>
2023-10-06 11:36:19 +0400

    add commit for finally on categories.js

--- src/assets/js/categories.js
+++ src/assets/js/categories.js
@@ -4,25 +4,25 @@ let categories = [
         id: 1,
         mainPhoto: "src/assets/img/amazon_product.jpg",
         name: 'phone',
-        img: "/src/assets/img/airconditioner_image.jpg",
+        photo: "/src/assets/img/airconditioner_image.jpg",
     },
     {
         id: 2,
         mainPhoto: "",
         name: 'TV',
-        img: "/src/assets/img/camera_image4.jpeg",
+        photo: "/src/assets/img/camera_image4.jpeg",
     },
     {
         id: 3,
         mainPhoto: "",
         name: "Air-conditioners",
-        img: "/src/assets/img/camera_image2.jpeg",
-    }, 
-     {
+        photo: "/src/assets/img/camera_image2.jpeg",
+    },
+    {
         id: 4,
         mainPhoto: "",
         name: "Air-conditioners",
-        img: "/src/assets/img/camera_image2.jpeg",
+        photo: "/src/assets/img/camera_image2.jpeg",
     },
 
 ]
@@ -32,126 +32,101 @@ var productsSurfaceInfo = [
     {
         id: 1,
         categoryId: 2,
-        info: {
-            price: 49.00,
-            discount: 44.00,
-            img: "./src/assets/img/beardcleaner.jpg",
-            hover: "./src/assets/img/beardsheaver.jpeg",
-        },
+        price: 49.00,
+        discount: 44.00,
+        photo: "/src/assets/img/beardcleaner.jpg",
+        hoverPhoto: "/src/assets/img/beardsheaver.jpeg",
         category: "Kitchen",
-        firstname: "Multigroomer All-in-One Trimmer Series 5000, 23 Piece Mens Grooming Kit",
-        href: "../src/assets/pages/products.html"
+        text: "Multigroomer All-in-One Trimmer Series 5000, 23 Piece Mens Grooming Kit",
     },
     {
         id: 2,
         categoryId: 2,
-        info: {
-            price: 249.00,
-            discount: 219.00,
-            img: "./src/assets/img/amazon_product.jpg",
-            hover: "./src/assets/img/background_amazon_product.jpg",
-        },
+        price: 249.00,
+        discount: 219.00,
+        photo: "/src/assets/img/amazon_product.jpg",
+        hoverPhoto: "/src/assets/img/background_amazon_product.jpg",
         category: "Kitchen",
-        firstname: "  Smart Speaker with Alexa Voice Control Built-in Compact Size with Incredible Sound for Any Room",
-        href: "../src/assets/pages/products.html"
+        text: "  Smart Speaker with Alexa Voice Control Built-in Compact Size with Incredible Sound for Any Room",
 
     },
     {
         id: 3,
         categoryId: 3,
-        info: {
-            price: 229.00,
-            discount: 209.00,
-            img: "./src/assets/img/amazon_product2.jpg",
-            hover: "./src/assets/img/background_amazon_product.jpg",
-        },
+        price: 229.00,
+        discount: 209.00,
+        photo: "/src/assets/img/amazon_product2.jpg",
+        hoverPhoto: "/src/assets/img/background_amazon_product.jpg",
         category: "Kitchen",
-        firstname: "  Home Speaker 500: Smart Bluetooth Speaker with Alexa Voice Control Built-In, White",
-        href: "../src/assets/pages/products.html"
+        text: "  Home Speaker 500: Smart Bluetooth Speaker with Alexa Voice Control Built-In, White",
 
     },
     {
         id: 4,
         categoryId: 2,
-        info: {
-            price: 1499.00,
-            discount: 1399.00,
-            img: "./src/assets/img/computer.jpg",
-            hover: "./src/assets/img/background_computer.jpg",
-        },
+        price: 1499.00,
+        discount: 1399.00,
+        photo: "/src/assets/img/computer.jpg",
+        hoverPhoto: "/src/assets/img/background_computer.jpg",
         category: "Laptop",
-        firstname: " 13 Ultrabook Gaming Laptop: Intel Core i7-1165G7 4 Core, NVIDIA GeForce GTX 1650 Ti Max-Q, 13.3″ 1080p 120Hz, 16GB RAM, 512GB SSD, CNC Aluminum, Chroma RGB, Thunderbolt 4",
-        img: "./src/assets/img/washingmachine_image.jpg",
-        href: "../src/assets/pages/products.html"
+        text: " 13 Ultrabook Gaming Laptop: Intel Core i7-1165G7 4 Core, NVIDIA GeForce GTX 1650 Ti Max-Q, 13.3″ 1080p 120Hz, 16GB RAM, 512GB SSD, CNC Aluminum, Chroma RGB, Thunderbolt 4",
 
     },
     {
         id: 5,
         categoryId: 2,
-        info: {
-            price: 1029.00,
-            discount: 999.00,
-            img: "./src/assets/img/computer2.jpg",
-            hover: "./src/assets/img/background_computer.jpg",
-        },
+        price: 1029.00,
+        discount: 999.00,
+        photo: "/src/assets/img/computer2.jpg",
+        hoverPhoto: "/src/assets/img/background_computer.jpg",
         category: "Laptop",
-        firstname: "15.6″ FHD Display Laptop – Intel i7 – Intel HD Graphics 6000 , Webcam, WiFi, Bluetooth, HDMI, Windows 11,Grey",
-        href: "../src/assets/pages/products.html"
+        text: "15.6″ FHD Display Laptop – Intel i7 – Intel HD Graphics 6000 , Webcam, WiFi, Bluetooth, HDMI, Windows 11,Grey",
 
     },
     {
         id: 6,
         categoryId: 2,
-        info: {
-            price: 699.00,
-            discount: 659.00,
-            img: "./src/assets/img/tablet.jpg",
-            hover: "./src/assets/img/phone_image.jpg",
-        },
+        price: 699.00,
+        discount: 659.00,
+        photo: "/src/assets/img/tablet.jpg",
+        hoverPhoto: "/src/assets/img/phone_image.jpg",
         category: "Laptop",
-        firstname: "Note 10 Pro 128GB 6GB RAM Factory Unlocked (GSM ONLY) International Model",
-        href: "../src/assets/pages/products.html"
+        text: "Note 10 Pro 128GB 6GB RAM Factory Unlocked (GSM ONLY) International Model",
 
     },
     {
         id: 7,
         categoryId: 1,
-        info: {
-            price: 1299.00,
-            discount: 1199.00,
-            img: "./src/assets/img/phone1.jpg",
-            hover: "./src/assets/img/phonebackground_img.jpg",
-        },
+        price: 1299.00,
+        discount: 1199.00,
+        photo: "/src/assets/img/phone1.jpg",
+        hoverPhoto: "/src/assets/img/phonebackground_img.jpg",
         category: "phone",
-        firstname: "5G Unlocked Smartphone,12GB RAM+256GB Storage120Hz Fluid Display Hasselblad Quad Camera 65W Ultra Fast Charge 50W Wireless Charge",
-        href: "../src/assets/pages/products.html"
+        text: "5G Unlocked Smartphone,12GB RAM+256GB Storage120Hz Fluid Display Hasselblad Quad Camera 65W Ultra Fast Charge 50W Wireless Charge",
 
     },
     {
         id: 8,
         categoryId: 1,
-        info: {
-            price: 1099.00,
-            discount: 999.00,
-            img: "./src/assets/img/phone2.jpg",
-            hover: "./src/assets/img/phonebackground_img.jpg",
-        },
+        price: 1099.00,
+        discount: 999.00,
+        photo: "/src/assets/img/phone2.jpg",
+        hoverPhoto: "/src/assets/img/phonebackground_img.jpg",
         category: "phone",
-        firstname: "5G Factory Unlocked Android Cell Phone 128GB Pro-Grade Camera 30X Space Zoom Night Mode, Space Grey",
-        href: "../src/assets/pages/products.html"
+        text: "5G Factory Unlocked Android Cell Phone 128GB Pro-Grade Camera 30X Space Zoom Night Mode, Space Grey",
 
     },
 ]
 
 
 
-document.addEventListener("DOMContentLoaded",function(){
-productInfo();
+document.addEventListener("DOMContentLoaded", function () {
+    productInfo();
 })
 
 function productInfo() {
-    let info_side_category=document.querySelector(".info_side");
-    let productCategory=document.querySelector("#products");
+    let info_side_category = document.querySelector(".info_side");
+    let productCategory = document.querySelector("#products");
 
     const productId = new URLSearchParams(window.location.search).get('id');
     if (!productId) {
@@ -168,122 +143,47 @@ function productInfo() {
         return;
     }
 
-        
-    info_side_category.innerHTML=`
+
+    info_side_category.innerHTML = `
         <div id="${product.id}">
         ${product.name}
    
         </div>`;
-        
 
 
-            // let id=productsSurfaceInfo.find(x=>x.categoryId);
-    
-        
-            // // console.log("hell")
-            // productCategory.innerHTML=`
-            // <div class="">
-            //     categoryId=${productcategory.categoryId}
-            //     <img src="${productcategory.img}" >
-            // </div>
-            // `;
+    let similarProducts = productsSurfaceInfo.filter(p => p.categoryId == product.id);
 
-            let similarProducts = productsSurfaceInfo.filter(p => p.categoryId == product.id);
-
-            console.log("mm")
-            for (let sm of similarProducts) {
-                let s = `
-                    <div class="">
-                    categoryId=${sm.categoryId}
-                    <img src="${sm.info.img}"> 
+    for (let item of similarProducts) {
+        let items = `
+                  
+                    <div class="card product-item" id="${item.categoryId}">
+                    <div class="card-photo">
+                        <img src="${item.photo}" class="card-img-top" alt="...">
+                        <img src="${item.hoverPhoto}" class="card-img-top img" alt="...">
+                    </div>    
+                        <i class="fa-solid fa-basket-shopping basket-icon"></i>
+                    <span class="sale">Sale</span>
+                    <div class="card-body">
+                        <h5 class="card-title"><i class="fa-regular fa-star fa-2xs"></i><i
+                                class="fa-regular fa-star fa-2xs"></i><i
+                                class="fa-regular fa-star fa-2xs"></i><i
+                                class="fa-regular fa-star fa-2xs"></i><i class="fa-regular fa-star fa-2xs"></i>
+                        </h5>
+                            <p class="card-text">${item.text}</p>
+                            <div class="price">
+                                <del>${item.price}$</del>
+                                <span>${item.discount}$</span>
+                            </div>
                     </div>
+                </div>
                 `;
-                productCategory.insertAdjacentHTML("beforeend", s);
-            }
-          
-
-    
-
+        productCategory.insertAdjacentHTML("beforeend", items);
+    }
 }
 
-     
-     
-
-// }
-
-// function getQueryParam(param) {
-        //     let urlParams = new URLSearchParams(window.location.search);
-        //     return urlParams.get(param);
-        // }
-        
-        // function productInfo() {
-        //     let categoryIdFromURL = getQueryParam("id"); // URL'den id parametresini alıyoruz
-        
-        //     // URL'den aldığımız id değerine sahip kategoriyi buluyoruz
-        //     let category = categories.find(x => x.id == categoryIdFromURL);
-        //     let productCategory = mainCategories.find(y => y.id == categoryIdFromURL);
-        
-        //     if (category && productCategory && category.name == productCategory.name) {
-        //         let info_side = document.querySelector(".info_side");
-        //         if(info_side) {
-        //             info_side.innerHTML = `${productCategory.name}`;
-        //         }
-        //     }
-        // }
-
-    // var mainCategories = [
-    //     {
-    //         id: 1,
-    //         mainPhoto: "",
-    //         name: 'Phone',
-    //         img: "./src/assets/img/airconditioner_image.jpg",
-    //     },
-    //     {
-    //         id: 2,
-    //         mainPhoto: "",
-    //         name: 'TV',
-    //         img: "./src/assets/img/airconditioner_image.jpg",
-    //     },
-    //     {
-    //         id: 3,
-    //         mainPhoto: "",
-    //         name: "Air-conditioners",
-    //         img: "./src/assets/img/airconditioner_image.jpg",
-    //     },
-    
-    // ]
+// <div class="">
+// categoryId=${item.categoryId}
+// <img src="${item.photo}">
+// </div>
 
 
-   // let elementsInfo = document.querySelector(".container .products");
-    // if (productsSurfaceInfo.categoryId == categories.id) {
-
-    //     for (let element of productsSurfaceInfo) {
-        //         let info = `
-        
-        //         <div class="card product-item" id="${element.id}">
-        //             <div class="card-photo">
-        //                 <img src="${element.info.img}" class="card-img-top" alt="...">
-        //                 <img src="${element.info.hover}" class="card-img-top img" alt="...">
-        //             </div>    
-        //                 <i class="fa-solid fa-basket-shopping basket-icon"></i>
-        //             <span class="sale">Sale</span>
-    //             <div class="card-body">
-    //                 <h5 class="card-title"><i class="fa-regular fa-star fa-2xs"></i><i
-    //                         class="fa-regular fa-star fa-2xs"></i><i
-    //                         class="fa-regular fa-star fa-2xs"></i><i
-    //                         class="fa-regular fa-star fa-2xs"></i><i class="fa-regular fa-star fa-2xs"></i>
-    //                 </h5>
-    //                 <a href="src/pages/product.html?id=${element.id}">
-    //                     <p class="card-text">${element.firstname}</p>
-    //                     <div class="price">
-    //                         <del>${element.info.price}$</del>
-    //                         <span>${element.info.discount}$</span>
-    //                     </div>
-    //                 </a>
-    //             </div>
-    //         </div>
-
-    //     `;
-    //         elementsInfo.insertAdjacentHTML("beforeend", info);
-    //     }
-    // }
