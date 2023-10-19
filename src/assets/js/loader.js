cbe6e6242d3f46a7cb4fb9a52d2a4dca30806114
BekirLee <nihadbekirli8@gmail.com>
2023-10-19 22:53:30 +0400

    add changes for new changes

--- src/assets/js/loader.js
+++ src/assets/js/loader.js
@@ -1,17 +1,34 @@
-document.querySelector(".card-width");
+
+let btn = document.querySelector(".logInBtn button");
+let card = document.querySelector(".card-width");
+// let loaderall = document.querySelector(".loader");
+let loader = document.querySelector(".loader");
+let animatebottom = document.querySelector(".animate-bottom");
+
+loader.style.display = "none";
+animatebottom.style.display = "none";
+
+deletesmthg();
+function deletesmthg() {
+
+
+    btn.addEventListener("click", function () {
+        animatebottom.style.display = "block";
+        loader.style.display = "block";
+
+        window.addEventListener('load', function () {
+
+            setTimeout(() => {
+                // loader.style.display = "none";
+                loader.classList.remove('active')
+
+            }, 1000);
+
+
+        });
+    })
+}
 
 
 
 
-
-
-window.addEventListener('load', function () {
-    let loader = document.querySelector(".loader");
-
-    setTimeout(() => {
-
-        loader.classList.remove('active')
-    }, 1000);
-
-
-});
