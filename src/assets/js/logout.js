5ecb8ab748e9ef6b1fd86dad09affeb85b1ccf8b
BekirLee <nihadbekirli8@gmail.com>
2023-10-28 01:58:59 +0400

    update loggin out from the page

--- src/assets/js/logout.js
+++ src/assets/js/logout.js
@@ -1,6 +1,6 @@
 
 let logoutbtn = document.querySelector(".logout");
-
+let lgout = document.getElementById("lgout");
 
 if (loggeduser && loggeduser.name) {
     logout.classList.remove("unactive");
@@ -10,5 +10,14 @@ if (loggeduser && loggeduser.name) {
         window.location.href = "/index.html";
     })
     groupClass.classList.add("unactive");
+
+    let userCardHTML = `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-text">User:${loggeduser.name}</h5>
+                <h5 class="card-text">Phone:${loggeduser.phone || "23434125"}</h5>
+            </div>
+        </div>`;
+    lgout.innerHTML = userCardHTML;
 
 }
